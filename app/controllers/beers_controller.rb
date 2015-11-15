6535c25cd919a196bf647db2680156aea86209dc
Mario Avila <avilamario16@gmail.com>
2015-11-14 22:52:52 -0600

    Added Beer relationships to CRUD

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -44,6 +44,6 @@ class BeersController < ApplicationController
 
   private
   def beer_params
-    params.require(:beer).permit(:name, :abv, :flavor, :color, :bubbly, :brewery, :style)
+    params.require(:beer).permit(:name, :abv, :flavor, :color, :bubbly, :brewery_id, :style_id)
   end
 end
